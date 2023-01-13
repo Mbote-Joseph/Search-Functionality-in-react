a384e02da2281c7f42686ecef99467c0e2f0b633
Mbote-Joseph <mbotejoseph001@gmail.com>
2023-01-13 22:24:12 +0300

    Search Functionality implementation in React

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import "./Search.css";
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
 
 let Search = ({ data }) => {
   const [searchTerm, setSearchTerm] = useState("");
   const [filteredData, setFilteredData] = useState(data);
   const [cart, setCart] = useState([]);
   //   const list = [];
+  const [isOpen, setIsOpen] = useState(false);
 
   //   console.log(data);
   const handleSearch = (e) => {
@@ -19,24 +23,39 @@ let Search = ({ data }) => {
   };
 
   //   Adding item to the Cart list
-  let onAdd = (item) => {
-    // item.preventDefault();
-
+  const onAdd = (item) => {
+    // e.preventDefault();
     setCart([
       ...cart,
-      {
-        id: item.id,
-        name: item.name,
-        imageUrl: item.imageUrl,
-        price: item.price,
-        description: item.description,
-      },
+      [
+        {
+          id: item.id,
+          name: item.name,
+          imageUrl: item.imageUrl,
+          price: item.price,
+          description: item.description,
+        },
+      ],
     ]);
     // console.log(list);
   };
 
   console.log(cart);
   console.log(cart.length);
+
+  if (cart.length > 0) {
+    let total = 0;
+    let prices = [];
+    for (let i = 0; i < cart.length; i++) {
+      prices.push(cart[i].price);
+    }
+
+    total = prices.reduce((acc, item) => {
+      return acc + item;
+    });
+
+    console.log(`Total Price ${total}`);
+  }
 
   // The rendering part
   return (
@@ -58,12 +77,29 @@ let Search = ({ data }) => {
               <img src={item.imageUrl} alt={item.name} />
               <p>{item.description}</p>
               <h5>${item.price}</h5>
-              <button className="btn btn-primary" onClick={onAdd}>
+              <button className="btn btn-primary" onClick={() => onAdd(item)}>
                 Add to Cart
               </button>
             </div>
           );
         })}
+      </div>
+      <div>
+        <button onClick={() => setIsOpen(true)}>Open Modal</button>
+        <Modal isOpen={isOpen} onRequestClose={() => setIsOpen(false)}>
+          <button onClick={() => setIsOpen(false)}>Close</button>
+          {filteredData.map((item) => {
+            // console.log(item);
+            return (
+              <div key={item.id} className="card-item">
+                <h4 className="card-header"> {item.name} </h4>
+                <img src={item.imageUrl} alt={item.name} />
+                <p>{item.description}</p>
+                <h5>${item.price}</h5>
+              </div>
+            );
+          })}
+        </Modal>
       </div>
     </div>
   );
--- src/components/data.js
+++ src/components/data.js
@@ -5,7 +5,7 @@ for (let i = 1; i <= 12; i++) {
     id: i,
     name: `Destination ${i}`,
     imageUrl: `https://picsum.photos/200?random=${i}`,
-    price: (i * 10).toFixed(2),
+    price: 45,
     description: `This is the description for destination ${i}. It's a great product that you will love.`,
   };
   items.push(item);
